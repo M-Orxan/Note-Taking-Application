16f0208e95089c7ec8143964b5fd7a1a99056be1
M-Orxan <orxanm385@gmail.com>
2022-12-02 23:58:17 +0400

    ConnectionWithSql

---
+++ Note-Taking App/Note-Taking App/DAL/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Note_Taking_App.DAL
+{
+    public class AppDbContext:DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- Note-Taking App/Note-Taking App/Program.cs
+++ Note-Taking App/Note-Taking App/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Note_Taking_App.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
 
 var app = builder.Build();
 
